e630155a7803f32ccb5859a510143cd5286bebb9
AntonMyshkevich <myshckevich.anton@yandex.ru>
2024-01-29 19:38:45 +0500

    комментарий для коммита, день8

---
+++ main.js
@@ -0,0 +1,20 @@
+let userName = 'Art';
+let userAge = 31;
+let userHeight;
+console.log(userName + ' is ' + userAge + ' years old');
+userHeight = 192;
+console.log(userHeight);
+userHeight = null; // Значение 192 у переменной userHeight заменилось на новое присвоенное null
+console.log(userHeight);
+console.log(1 + 5); 
+console.log(1 - 4); 
+console.log(4 / 0); // infinity
+console.log(-4 / 0); // -infinity
+console.log(3 * 999999999999999+3); // численный предел
+console.log(`2 + 2 = ${2+2}`)
+console.log( 5 % 2 );
+console.log( 2 ** 8 );
+console.log( 2 + true );
+let x; //undefined
+console.log(x+1); // undefined
+console.log(x+String(1)); //будет undefined1
